cbe0e2d7aba832f48dcea83e6073f73d1c06febc
mclemensat <marieclemensat@wanadoo.fr>
2022-03-15 14:03:41 +0100

    them 3 + form + fichier js

---
+++ script.js
@@ -0,0 +1,9 @@
+const message =
+  "Merci pour ton message ! Il a bien été envoyé à l'équipe du Carré";
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert(message);
+  });
