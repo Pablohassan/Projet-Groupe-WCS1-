659484ef4c6e5c85fc38c417842b8af932865d82
mclemensat <marieclemensat@wanadoo.fr>
2022-03-17 15:47:23 +0100

    reso conflits v2

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const langE1 = document.querySelector(".langWrap");
+const link = document.querySelectorAll("a");
+const titleE1 = document.querySelector(".title");
+const texteE1 = document.querySelector(".texte");
+
+link.forEach(langue => {
+  langue.addEventListener('click', () => {
+    langE1.querySelector(".active").classList.remove("active");
+    langue.classList.add("active");
+
+    const attribut = langue.getAttribute("language");
+
+    titleE1.textContent = data[attribut].title;
+    texteE1.textContent = data[attribut].texte;
+  });
+});
+
+const data = {
+  "francais":
+  {
+    "title": "Bonjour le monde",
+    "texte": "Coucou à tous et bienvenue"
+  },
+  "italiano":
+  {
+    "title": "Ciao mondo",
+    "texte": "Ciao a tutti e benvenuti"
+  }
+}
+
 document
   .getElementById("contactForm")
   .addEventListener("submit", function (event) {
